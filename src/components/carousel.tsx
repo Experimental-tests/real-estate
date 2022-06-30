91f37ae9a4504ac5eeb9d9fc9f4f76118c81030b
Benrandja Akram <a.benrandja@gmail.com>
2022-06-30 16:26:11 +0100

    Some ui tweaks

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -4,61 +4,52 @@ import '@egjs/react-flicking/dist/flicking.css'
 import { NextICon, PrevIcon } from 'ui/icons'
 
 const Carousel: React.FC<React.PropsWithChildren<{}>> = ({ children }) => {
-  // current index 2 since the visible panels are 3
-  const [currentIndex, setCurrentIndex] = useState(2)
   const ref = useRef<any>()
   const [mounted, setMounted] = useState(false)
 
   useEffect(() => {
-    // run this component on the client side
+    // run this component only the client side
     setMounted(true)
   }, [])
 
-  useEffect(() => {
-    if (!mounted) return
-    const listener = (props: any) => {
-      setCurrentIndex([...props.visiblePanels].pop().index)
-    }
-    const carousel = ref.current!
-    carousel.on('visibleChange', listener)
-
-    return () => carousel.off('visibleChange', listener)
-  }, [mounted])
-
-  // run this component on the client side
+  // run this component only the client side
   if (!mounted) return null
   return (
     <div className="relative -mx-6 -my-6">
-      <button
-        onClick={async () => {
-          // catch animation errors when user clicks next and the animation is still not finished
-          try {
-            await ref.current?.prev()
-          } catch (error) {}
-        }}
-        className="absolute top-0 -left-2 bottom-0 disabled:opacity-60"
-        disabled={currentIndex === 2}
-      >
-        <PrevIcon />
-      </button>
-      <button
-        onClick={async () => {
-          // catch animation errors when user clicks next and the animation is still not finished
-          try {
-            await ref.current?.next()
-          } catch (error) {}
-        }}
-        className="absolute top-0 -right-2 bottom-0 disabled:opacity-50"
-        disabled={currentIndex === 4}
-      >
-        <NextICon />
-      </button>
+      <div className="absolute top-0 -left-2 w-6 bg-white z-50 flex items-center   bottom-0 ">
+        <button
+          onClick={async () => {
+            // catch animation errors when user clicks next and the animation is still not finished
+            try {
+              await ref.current?.prev()
+            } catch (error) {}
+          }}
+          className="z-50"
+        >
+          <PrevIcon />
+        </button>
+      </div>
+      <div className="absolute top-0 -right-2 w-6 bg-white z-50 flex items-center justify-end  bottom-0 ">
+        <button
+          onClick={async () => {
+            // catch animation errors when user clicks next and the animation is still not finished
+            try {
+              await ref.current?.next()
+            } catch (error) {}
+          }}
+          className="z-50"
+        >
+          <NextICon />
+        </button>
+      </div>
+
       <Flicking
         useResizeObserver
         panelsPerView={3}
         ref={ref}
         align="prev"
         bound
+        circular
       >
         {children}
       </Flicking>
--- src/ui/layout.tsx
+++ src/ui/layout.tsx
@@ -4,18 +4,18 @@ import Logo from '../components/logo'
 const Layout: React.FC<React.ComponentProps<'div'>> = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen">
-      <header className="flex  items-center h-[115px] shadow-header ">
+      <header className="sticky top-0 z-20 bg-white flex items-center h-[115px] shadow-header ">
         <div className="w-[450px] pl-20 2xl:w-[600px]">
           <MenuIcon />
         </div>
-        <div className="pl-12 flex  justify-center items-center">
+        <div className="pl-12 absolute inset-0 flex justify-center items-center">
           <Logo />
         </div>
       </header>
       <main className="flex-1 flex">
         <img
           src="/map.png"
-          className="object-cover w-[450px] 2xl:w-[600px] max-h-screen sticky top-0"
+          className="object-cover w-[400px] xl:w-[595px] h-[calc(100vh-115px)] sticky top-[115px]"
           alt="map"
         />
         <div className="px-16 py-6 overflow-hidden flex flex-col">
